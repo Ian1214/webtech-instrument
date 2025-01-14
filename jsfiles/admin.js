14709521c72288d8746e497addeb290bb679b49d
Ian1214 <act.iancadiz@gmail.com>
2025-01-15 07:52:13 +0800

    Update admin.js

--- jsfiles/admin.js
+++ jsfiles/admin.js
@@ -106,7 +106,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (isFutureDate(sDate.value)) {
-      showCustomAlert(`${item.name}The selected date cannot be in the future.');
+      showCustomAlert(`${item.name}The selected date cannot be in the future.'`);
         return;
     }
 
